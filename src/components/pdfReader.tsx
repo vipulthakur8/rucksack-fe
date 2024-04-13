fd80fad0bcf16036c7b635c45a269d9c8e699637
Vipul Thakur <vipulthakurofficial@gmail.com>
2024-04-13 17:10:39 +0530

    pdf reader done

--- src/components/pdfReader.tsx
+++ src/components/pdfReader.tsx
@@ -3,7 +3,9 @@
 // import axios from "axios"
 import { useEffect, useState } from "react"
 // import { Document, Page } from 'react-pdf'
-import { Document, Page } from '@react-pdf/renderer';
+// import { Document, Page } from '@react-pdf/renderer';
+import { Viewer, Worker } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
 
 export default function PDFReader (props:any) {
 
@@ -34,31 +36,25 @@ export default function PDFReader (props:any) {
                         <button className="btn btn-sm">Close</button>
                     </div>
                 </div>
-                <div>
+                <div className="pt-5">
                     {
                         pdf && (
-                            // <Document file={{data: pdf}} >
-                            //     <Page size={'A4'} pageNumber={1} />
-                            // </Document>
-                            // <object
-                            //     width={'100%'}
-                            //     height={'100%'}
-                            //     type="applicaiont/pdf"
-                            //     data={URL.createObjectURL(pdf)}
-
-                            // >
-                            //     <p className="text-center mt-[10rem] text-red">
-                            //         PDF viewer not available. Please download the PDF to view it.
-                            //     </p>
-                            // </object>
-                            <embed
-                            src={URL.createObjectURL(pdf)}
-                            type="application/pdf"
-                            width="100%"
-                            height="500px"
-                          />
+                            <Worker 
+                            workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js"
+                            >
+                                <div style={{
+                                      border: '1px solid rgba(0, 0, 0, 0.3)',
+                                      height: '750px',
+                                }}>
+                                    <Viewer 
+                                    fileUrl={URL.createObjectURL(pdf)} 
+                                    />
+                                </div>
+                              
+                            </Worker>
                         )
                     }
+
                 </div>
             </div>
         )
